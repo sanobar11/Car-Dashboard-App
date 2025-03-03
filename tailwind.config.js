822dd8c95609f8a37fb4e38ef155b026fd0cd025
Sanobar unissa <sanobarunissa@gmail.com>
2025-03-04 00:36:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'my-font':['Dancing Script','Tangerine','Bebas Neue','Caveat','Abel','Major Mono Display']
+      }
+    },
+  },
+  plugins: [],
+}
+
